132274b17a4589730fee1aded8ee0506c79aebfa
white <white.seba@hotmail.com>
2020-06-22 06:54:03 -0300

    Refactor classes

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 from random import randint
 
 class Character:
+    
     def __init__(self, name, attack, magic_attack, armor, magic_armor, hp_max, level):
         self.name = name
         self.attack = attack
@@ -18,6 +19,7 @@ class Character:
             
 
 class Monster(Character):
+    
     def __init__(self, name, attack, magic_attack, armor, magic_armor, hp_max, exp):
         super().__init__(name, attack, magic_attack, armor, magic_armor, hp_max, exp, level=1)
 
@@ -33,8 +35,11 @@ class Player(Character):
         self.crit_damage = crit_damage
         self.speed = speed
         self.hp = hp
+        self.inventory = []
 
-        #self.inventory = [Items.potion, Items.mana]
+    def show_inventory(self):
+        for item in self.inventory:
+            f"{item}"
 
     def equip_item(self, item):
         # Si el item no está equipado
@@ -51,12 +56,16 @@ class Player(Character):
             self.crit_damage += item.crit_damage
             self.speed += item.speed
             self.hp_max += item.hp_max
+        else:
+            print("Este item ya esta equipado.")
     
     def use_item(self, item):
-        if item.type == "Consumable":
+        if item.type == "C": # Consumable
             self.inventory # remover item del inventario (completar cuando tenga internet, no recuerdo como remover de listas)
             self.mana += item.mana
             self.hp += item.hp
+        else:
+            print("Este item no se puede usar.")
             
     def move(self):
         pass
@@ -71,9 +80,32 @@ class Player(Character):
 
 
 class Item:
-    def __init__(self, name, mana, hp, value):
+    
+    def __init__(self, name, value):
         self.name = name
-        self.mana = mana
-        self.hp = hp
         self.value = value
+
+class Consumables(Item):
+
+    def __init__(self, name, value, hp, mana, type="C"):
+        super().__init__(name, value)
+        self.hp = hp
+        self.mana = mana
+
+
+class Weapons(Item):
+    
+    def __init__(self, name, value, attack, magic_attack, crit_chance, crit_damage, state):
+        super().__init__(name, value)
+        self.attack = attack
+        self.magic_attack = magic_attack
+        self.crit_chance = crit_chance
+        self.crit_damage = crit_damage
+        self.state = state
+        
+
+asd = Weapons(1, 1, 1, 1, 1, 1, True)
+#asd.state = True
+print(asd.state)
+
 
